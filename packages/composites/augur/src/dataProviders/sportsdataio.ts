8f40c169293605bbf5aa85abdf69f87c5f3dc469
robert <robert.davidson@forecastfoundation.org>
2021-07-08 19:25:59 -0700

    fix mma fight query. add some logging

--- packages/composites/augur/src/dataProviders/sportsdataio.ts
+++ packages/composites/augur/src/dataProviders/sportsdataio.ts
@@ -302,9 +302,12 @@ export const createFighter: Execute = async (input) => {
 
   const leagues = ["UFC"]
   for (const league of leagues) {
-    const schedule = (await getFightSchedule(input.id, sport, league, `${new Date().getFullYear()}`, sportsdataioExec))
+    const season = new Date().getFullYear();
+    Logger.debug(`Getting fight schedule for league ${league} in season ${season}.`)
+    const schedule = (await getFightSchedule(input.id, sport, league, `${season}`, sportsdataioExec))
       .filter(event => event.Status === "Scheduled")
 
+    Logger.debug(`Getting ${schedule.length} events from season, then filtering out unscheduled`);
     for (const event of schedule) {
       const eventFights = (await getFights(input.id, sport, event.EventId, sportsdataioExec))
         .filter(fight => fight.Status === "Scheduled")
@@ -436,111 +439,11 @@ export const resolveFight: Execute = async (input) => {
 
   const fightId = Number(validator.validated.data.eventId)
   const sport = validator.validated.data.sport
+
   const sportsdataioExec = Sportsdataio.makeExecute(Sportsdataio.makeConfig(Sportsdataio.NAME))
 
-  let fight;
-  if(parseInt(fightId) === 2131) {
-    fight = {
-      FightId: 2131,
-      Order: 1,
-      Status: 'Final',
-      WeightClass: 'Lightweight',
-      CardSegment: 'Main Card',
-      Referee: null,
-      Rounds: 5,
-      ResultClock: 300,
-      ResultRound: 5,
-      ResultType: "Decision - Unanimous",
-      WinnerId: 140000266,
-      Active: true,
-      FightStats: [
-        {
-          FighterId: 140000266,
-          FirstName: 'Dustin',
-          LastName: 'Poirier',
-          Winner: false,
-          FantasyPoints: 0.0,
-          FantasyPointsDraftKings: 0.0,
-          Knockdowns: 0.0,
-          TotalStrikesAttempted: 0.0,
-          TotalStrikesLanded: 0.0,
-          SigStrikesAttempted: 0.0,
-          SigStrikesLanded: 0.0,
-          TakedownsAttempted: 0.0,
-          TakedownsLanded: 0.0,
-          TakedownsSlams: 0.0,
-          TakedownAccuracy: 0.0,
-          Advances: 0.0,
-          Reversals: 0.0,
-          Submissions: 0.0,
-          SlamRate: 0.0,
-          TimeInControl: 0.0,
-          FirstRoundWin: false,
-          SecondRoundWin: false,
-          ThirdRoundWin: false,
-          FourthRoundWin: false,
-          FifthRoundWin: false,
-          DecisionWin: false,
-        },
-        {
-          FighterId: 140000867,
-          FirstName: 'Conor',
-          LastName: 'McGregor',
-          Winner: false,
-          FantasyPoints: 0.0,
-          FantasyPointsDraftKings: 0.0,
-          Knockdowns: 0.0,
-          TotalStrikesAttempted: 0.0,
-          TotalStrikesLanded: 0.0,
-          SigStrikesAttempted: 0.0,
-          SigStrikesLanded: 0.0,
-          TakedownsAttempted: 0.0,
-          TakedownsLanded: 0.0,
-          TakedownsSlams: 0.0,
-          TakedownAccuracy: 0.0,
-          Advances: 0.0,
-          Reversals: 0.0,
-          Submissions: 0.0,
-          SlamRate: 0.0,
-          TimeInControl: 0.0,
-          FirstRoundWin: false,
-          SecondRoundWin: false,
-          ThirdRoundWin: false,
-          FourthRoundWin: false,
-          FifthRoundWin: false,
-          DecisionWin: false,
-        },
-      ],
-      Fighters: [
-        {
-          FighterId: 140000266,
-          FirstName: 'Dustin',
-          LastName: 'Poirier',
-          PreFightWins: 27,
-          PreFightLosses: 6,
-          PreFightDraws: 0,
-          PreFightNoContests: 0,
-          Winner: false,
-          Moneyline: -132,
-          Active: true,
-        },
-        {
-          FighterId: 140000867,
-          FirstName: 'Conor',
-          LastName: 'McGregor',
-          PreFightWins: 22,
-          PreFightLosses: 5,
-          PreFightDraws: 0,
-          PreFightNoContests: 0,
-          Winner: false,
-          Moneyline: 108,
-          Active: true,
-        },
-      ],
-    }
-  } else {
-    fight = await getFight(input.id, sport, fightId, sportsdataioExec)
-  }
+  Logger.debug(`Getting fight ${input.id} for sport ${sport}, which has fightId ${fightId}`);
+  const fight = await getFight(input.id, sport, fightId, sportsdataioExec)
 
   if (!fight) {
     throw Error(`Unable to find fight ${fightId}`)
